76ff41f3ce1e97a29274c642766485581231006c
arpsoid <ar0@krc.karelia.ru>
2022-01-26 09:42:00 +0300

    Update script.py

--- QA4QUBO/script.py
+++ QA4QUBO/script.py
@@ -5,14 +5,8 @@ import dwave.inspector as inspector
 from dwave.system import DWaveSampler, EmbeddingComposite
 import dimod
 
-def annealer(theta, sampler, k, time=False):
-    if time:
-        start = time.time()
-        
-    response = sampler.sample_qubo(theta, num_reads=k) 
-    
-    if time:
-        print(f"Time: {time.time()-start}")
+def annealer(h, J, sampler, k, time=False):
+    response = sampler.sample_ising(h, J, num_reads=k) 
     
     return list(response.first.sample.values())
 

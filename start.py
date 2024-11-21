284c0bbf4fc92acecc767c41226b275b1176540e
MalMariia97 <mariia.maltseva1997@gmail.com>
2024-11-21 11:33:34 +0300

    Update start.py

--- start.py
+++ start.py
@@ -13,16 +13,18 @@ import csv
 np.set_printoptions(threshold=sys.maxsize)
 
 def main():    
-    _Q=[[0, -0.2, 0, 0],
-        [0, 1.0, -0.5, 1.0],
-        [0, 0, -0.9, 0],
-        [0, 0, 0, 0.7]]
-    nn = 4
+    _Q=[[0, -0.2, 0, 0, 1],
+        [0, 1.0, -0.5, 1.0, 0.5],
+        [0, 0, -0.9, 0, 0.1],
+        [0, 0, 0, 0.7, 0.3],
+        [0.1, 0, 0, 0.7, 0.1]]
+    nn = 5
 
-    for numberofiters in range(10):
-        z, r_time = solver.solve(d_min = 70, eta = 0.2, i_max = 200, k = 1, lambda_zero = 3/2, n = nn, 
-        N = 10, N_max = 100, p_delta = 0.01, q = 0.99,#0.2, 
+    for numberofiters in range(1):
+        z, f_star, r_time = solver.solve(d_min = 7, eta = 0.02, i_max = 200, k = 1000, lambda_zero = 3/2, n = nn, 
+        N = 10, N_max = 100, p_delta = 0.1, q = 0.99,#0.2, 
         topology = 'pegasus', Q = _Q, log_DIR = "", sim = True)
 
+        print(z, f_star)
 if __name__ == '__main__':
     main()

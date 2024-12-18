61268a7daf4aec26137330e9bb6ce4e830c06022
MakarMariia97 <mariamaltseva1997@gmail.com>
2024-12-18 12:00:03 +0300

    Update script.py

--- QA4QUBO/script.py
+++ QA4QUBO/script.py
@@ -1,33 +1,16 @@
-import numpy as np
-import time
-import datetime
-import dwave.inspector as inspector
-from dwave.system import DWaveSampler, EmbeddingComposite
-import dimod
 from QA4QUBO import solver
+from utils_qals import check_validity
 
-def annealer(h, J, sampler, k, cnf, time=False):
+def annealer(h, J, sampler, k, cnf):
     ifsat = False
-    N=10
+    N=1 # repeat sampling several times (because of the probabilistic nature of annealing)
     indN=0
     while (not ifsat) and indN<N:
-  #      print(indN)
-     #   qpu = DWaveSampler()
-      #  long_time = qpu.properties["annealing_time_range"][1]
         response = sampler.sample_ising(h, J, num_reads=k, chain_strength = 10) 
         sol = response.first.sample.values()
-        qubo_sol = solver.ising_qubo(list(sol))
-        cnf_sol = cnf.convert_solution(qubo_sol)
-        ifsat = cnf.is_solution_valid(cnf_sol)
-  #      print(sol)
-        if ifsat:
-            print("solution valid")
+        qubo_sol = solver.ising_qubo(list(sol)) # convert spins to boolean variables
+        ifsat = check_validity(qubo_sol,cnf) # check if cnf satisfiable
         indN+=1
-    
+
     return list(sol)
-
-def hybrid(theta, sampler):
-    response = sampler.sample_qubo(theta)
-
-    return list(response.first.sample.values())
     
